4c03db50313775e07224c3f78e488fd2e568a78f
Nadish Sood <nadishsood@gmail.com>
2020-06-18 14:37:42 -0400

    upto lesson 4

---
+++ contextapp/src/components/BookList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { ThemeContext } from "./../context/ThemeContext";
+
+class BookList extends Component {
+    static contextType = ThemeContext;
+
+    render() { 
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+            <ul>
+              <li style={{background: theme.ui}}>Harry Potter</li>
+              <li style={{background: theme.ui}}>Moth Smoke</li>
+              <li style={{background: theme.ui}}> Homo Sapiens</li>
+              
+            </ul>
+          </div>
+        );
+    }
+}
+ 
+export default BookList;
